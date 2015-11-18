59b7d7bc408b614488d0181be3ecec7047bf84b2
MikeDX <mikedx@gmail.com>
2015-11-18 00:08:05 +0000

    improving runner

--- src/runner/r.c
+++ src/runner/r.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
+#ifdef WIN32
+#define ide "system\\"TARGET
+#define dbg "system\\"RDEBUG
+#else
+#define ide "system/"TARGET
+#define dbg "system/"RDEBUG
 
+#endif
 
 int main(void) {
-int ret = system("./system/div INIT");
-while(ret==256 || ret == 512) {
-//	printf("%d returned\n",ret);
+printf("[%s]\n",ide);
+printf("[%s]\n",dbg);
 
-	if(ret==256) {
-		system("./system/divdbg system/EXEC.EXE");
-		ret=system("./system/div");
+int ret = system(ide " INIT");
+//printf("%d returned\n",ret);
+
+while(ret == 1 || ret ==2 || ret==256 || ret == 512) {
+
+	if(ret == 1 || ret==256) {
+		system( dbg " system/EXEC.EXE");
+		ret=system(ide);
 	}
 
-	if(ret==512) {
-		ret=system("./system/div TEST");
+	if(ret == 2 || ret==512) {
+		ret=system(ide " TEST");
 	}
 }
 return ret;
