62a8c394f0a74ea5868c32dea9c0fbb598834984
MikeDX <mikedx@gmail.com>
2016-02-03 01:42:22 +0000

    adding widescreen ide video mdoes

--- src/det_vesa.c
+++ src/det_vesa.c
@@ -33,14 +33,17 @@ void detectar_vesa(void) { // Detects available video modes
   short *modelist;
   int n;
 
-  num_modos=6;
+  num_modos=8;
   modos[0].ancho=320; modos[0].alto=240; modos[0].modo=0;
   modos[1].ancho=640; modos[1].alto=480; modos[1].modo=1;
   modos[2].ancho=800; modos[2].alto=600; modos[2].modo=1;
   modos[3].ancho=1024; modos[3].alto=768; modos[3].modo=1;
   modos[4].ancho=1280; modos[4].alto=1024; modos[4].modo=1;
-  modos[5].ancho=376; modos[5].alto=282; modos[5].modo=1;
-  modos[5].ancho=376; modos[5].alto=282; modos[5].modo=1;
+  modos[5].ancho=1920; modos[5].alto=1080; modos[5].modo=1;
+  modos[6].ancho=1280; modos[6].alto=720; modos[6].modo=1;
+  
+//  modos[5].ancho=376; modos[5].alto=282; modos[5].modo=1;
+  modos[7].ancho=376; modos[7].alto=282; modos[7].modo=1;
 
 
 sprintf(marcavga,"SDL Video Driver ");
