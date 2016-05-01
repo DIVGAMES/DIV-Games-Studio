f8aa2ee00a0c55567bc5a71a8e296f79776826aa
MikeDX <mikedx@gmail.com>
2016-05-01 19:56:43 +0100

    cleanup / instructions for pi cross compile

--- tools/pi.cmake
+++ tools/pi.cmake
@@ -1,9 +1,68 @@
-# Sample toolchain file for building for Windows from an Ubuntu Linux system.
+# Sample toolchain file for building for Raspberry pi from an Ubuntu Linux system.
 #
-# Typical usage:
-#    *) install cross compiler: `sudo apt-get install mingw-w64 g++-mingw-w64`
-#    *) cd build
-#    *) cmake -DCMAKE_TOOLCHAIN_FILE=~/Toolchain-Ubuntu-mingw64.cmake ..
+# mkdir ~/raspidev
+# cd ~/raspidev
+# git clone https://github.com/raspberrypi/tools.git
+# export ARCH=arm
+# export CROSS_COMPILE=arm-bcm2708hardfp-linux-gnueabi-
+# export CC=~/raspidev/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-gcc
+# export NM=~/raspidev/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-nm
+# export LD=~/raspidev/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-ld
+# export CXX=~/raspidev/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-g++
+# export RANLIB=~/raspidev/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-ranlib
+# export AR=~/raspidev/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-ar
+#
+# wget http://www.libsdl.org/release/SDL-1.2.15.tar.gz
+# tar xf SDL-1.2.15.tar.gz
+# 
+
+## build SDL1.2
+# cd SDL-1.2.15
+# ./configure --disable-pulseaudio --target=arm-linux --host=arm-linux --prefix=/home/mike/raspidev/SDL_cross
+# make
+# make install
+#
+
+## build zlib
+# cd ..
+# wget zlib.net/zlib-1.2.8.tar.gz
+# tar xf zlib-1.2.8.tar.gz
+# cd zlib-1.2.8
+# ./configure --prefix=/home/mike/raspidev/SDL_cross
+# make
+# make install
+#
+#
+
+## build mikmod
+# cd ..
+# wget http://netix.dl.sourceforge.net/project/mikmod/libmikmod/3.3.8/libmikmod-3.3.8.tar.gz
+# tar xf libmikmod-3.3.8.tar.gz
+# cd libmikmod-3.3.8
+# ./configure --disable-pulseaudio --target=arm-linux --host=arm-linux --prefix=/home/mike/raspidev/SDL_cross
+# make
+# make install
+
+## build SDL_mixer
+# cd ..
+# wget https://www.libsdl.org/projects/SDL_mixer/release/SDL_mixer-1.2.12.tar.gz
+# tar xf SDL_mixer-1.2.12.tar.gz
+# cd SDL_mixer-1.2.12
+# export PATH=~/raspidev/SDL_cross/bin:$PATH
+# ./configure --disable-pulseaudio --target=arm-linux --host=arm-linux --prefix=/home/mike/raspidev/SDL_cross
+#
+### edit Makefile, change the EXTRA_LDFLAGS LINE to tell sdl_mixer where to find libSDL
+### < EXTRA_LDFLAGS =  -lSDL -lm
+### ---
+### > EXTRA_LDFLAGS =  -L$(prefix)/lib -lSDL -lm
+#
+# make
+# make install
+#
+#
+## You should now be able to cross compile / build for pi!
+
+SET(CROSS_DIR "/home/mike/raspidev/")
 
 set(CMAKE_SYSTEM_NAME PI)
 SET(PLATFORM "PI")
@@ -20,7 +79,7 @@ SET(HAS_DLL 1)
 SEt(HAS_FLI 1)
 
 SET(OS_DEFINITIONS " -D_GNU_SOURCE=1 -D_REENTRANT ")
-set(TOOLCHAIN_PREFIX /home/mike/raspidev/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi)
+set(TOOLCHAIN_PREFIX ${CROSS_DIR}tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi)
 
 # cross compilers to use for C and C++
 set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
@@ -30,28 +89,10 @@ set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
 # target environment on the build host system
 #   set 1st to dir with the cross compiler's C/C++ headers/libs
 set(CMAKE_FIND_ROOT_PATH ~/raspidev/ )
-set(CMAKE_SYSTEM_LIBRARY_PATH "/home/mike/raspidev/tools/arm-bcm2708/arm-bcm2708-linux-gnueabi/arm-bcm2708-linux-gnueabi/sysroot/usr/" )
-
-#/${TOOLCHAIN_PREFIX})
-
-# modify default behavior of FIND_XXX() commands to
-# search for headers/libs in the target environment and
-# search for programs in the build host environment
+set(CMAKE_SYSTEM_LIBRARY_PATH ${CROSS_DIR}tools/arm-bcm2708/arm-bcm2708-linux-gnueabi/arm-bcm2708-linux-gnueabi/sysroot/usr/ )
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-include_directories("/home/mike/raspidev/SDL_cross/include/")
+include_directories(${CROSS_DIR}SDL_cross/include/)
 
-#TARGET_LINK_LIBRARIES ("mingw32 SDLmain  SDL SDL_Mixer")
-#-mwindows /usr/i686-w64-mingw32/lib/x64/SDL_mixer.lib")
-#set ( CMAKE_EXE_LINKER_FLAGS "-lmingw32 -lSDLmain  -lSDL -mwindows")
-
-#ADD_DEFINITIONS( -DRPI  )
-set ( CMAKE_EXE_LINKER_FLAGS "-L/home/mike/raspidev/SDL_cross/lib -Wl,-rpath,/home/mike/raspidev/SDL_cross/lib -lSDL -lpthread -ldl")
-#set ( CMAKE_EXE_LINKER_FLAGS "-lmad" )
-
-# -DGCW_SOFTSTRETCH )
-#-DMIXER) 
-
-#SET(OSDEP "src/osdep/osdep.c" )
-
+set ( CMAKE_EXE_LINKER_FLAGS "-L${CROSS_DIR}SDL_cross/lib -Wl,-rpath,/home/mike/raspidev/SDL_cross/lib -lSDL -lpthread -ldl")
