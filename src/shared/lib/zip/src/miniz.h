21a6c2e72ddded4ccd817baf0af5a2b6f1556e6b
Mike DX <mikedx@gmail.com>
2017-01-05 12:54:08 +0000

    Fixed minizip header to work with devkitPro (missing utime)

--- src/shared/lib/zip/src/miniz.h
+++ src/shared/lib/zip/src/miniz.h
@@ -191,7 +191,7 @@
 // functions (such as tdefl_compress_mem_to_heap() and tinfl_decompress_mem_to_heap()) won't work.
 //#define MINIZ_NO_MALLOC
 
-#if defined(__TINYC__) && (defined(__linux) || defined(__linux__))
+#if defined( N3DS ) || ( defined(__TINYC__) && (defined(__linux) || defined(__linux__)))
   // TODO: Work around "error: include file 'sys\utime.h' when compiling with tcc on Linux
   #define MINIZ_NO_TIME
 #endif
